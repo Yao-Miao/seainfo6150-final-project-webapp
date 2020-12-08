650516ed95ff9025ec2e78a3d838b14fa8267369
Yao-Miao <miaoyaoapply@outlook.com>
2020-12-08 00:32:07 -0800

    test search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,20 @@
 import React from "react";
 import classes from "./Search.module.css";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const search = (props) => {
   let categoryInput;
-
-  function clickSearch (){
+  function clickSearch() {
     console.log(categoryInput.value);
-    props.history.push("/category/" + categoryInput.value);
-  };
+    //props.history.push("/category/" + categoryInput.value);
+    props.history.push("/category/salad");
+  }
+  /*
+  <button className={classes.btn} onClick={clickSearch}>
+          <BiSearchAlt2 />
+          <span>Search</span>
+        </button>*/
   return (
     <div className={classes.SearchDiv}>
       <form className={classes.Search} onSubmit={clickSearch}>
